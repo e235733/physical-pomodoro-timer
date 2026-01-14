f1e18eded5a91305aa3eb7056a336705d0e6e190
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-01-14 17:47:07 +0900

    feat: implement start button

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,22 @@
 
 SevSeg sevseg;
 
+const int PIN_BTN_START = 2;
+const int PTN_BTN_RESET = 3;
+
+bool lastStartBtnState = LOW;
+
+int count = 0;
+
+
 void setup() {
+  // start serial communication
+  Serial.begin(9600);
+
+  // button settings
+  pinMode(PIN_BTN_START, INPUT_PULLUP);
+  pinMode(PIN_BTN_START, INPUT_PULLUP);
+
   // display settings
   byte numDigits = 4;
   byte digitPins[] = {22, 23, 24, 25};
@@ -14,6 +29,12 @@ void setup() {
 
 void loop() {
   // display
-  sevseg.setNumber(1234, 1);
+  sevseg.setNumber(count);
   sevseg.refreshDisplay();
+
+  // checking start button
+  if (digitalRead(PIN_BTN_START) == LOW && lastStartBtnState == HIGH) {
+    count++;
+  }
+  lastStartBtnState = digitalRead(PIN_BTN_START);
 }
